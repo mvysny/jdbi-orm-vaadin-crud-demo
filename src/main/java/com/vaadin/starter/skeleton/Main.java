05a9586c5bd0f802c4fb990b67fce5acca2d41b6
Martin Vysny <mavi@vaadin.com>
2022-11-01 21:38:18 +0200

    ported to Vaadin Boot

--- src/main/java/com/vaadin/starter/skeleton/Main.java
+++ src/main/java/com/vaadin/starter/skeleton/Main.java
@@ -1,87 +1,14 @@
 package com.vaadin.starter.skeleton;
 
-import com.vaadin.flow.server.VaadinServlet;
-import com.vaadin.flow.server.startup.ServletContextListeners;
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.util.resource.Resource;
-import org.eclipse.jetty.webapp.*;
-
-import java.net.MalformedURLException;
-import java.net.URL;
+import com.github.mvysny.vaadinboot.VaadinBoot;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * Run {@link #main(String[])} to launch your app in Embedded Jetty.
  * @author mavi
  */
 public final class Main {
-
-    private static Server server;
-
-    public static void main(String[] args) throws Exception {
-        start(args);
-        server.join();
-    }
-
-    public static void start(String[] args) throws Exception {
-        // change this to e.g. /foo to host your app on a different context root
-        final String contextRoot = "/";
-
-        // detect&enable production mode
-        if (isProductionMode()) {
-            // fixes https://github.com/mvysny/vaadin14-embedded-jetty/issues/1
-            System.out.println("Production mode detected, enforcing");
-            System.setProperty("vaadin.productionMode", "true");
-        }
-
-        final WebAppContext context = new WebAppContext();
-        context.setBaseResource(findWebRoot());
-        context.setContextPath(contextRoot);
-        context.addServlet(VaadinServlet.class, "/*");
-        context.setAttribute("org.eclipse.jetty.server.webapp.ContainerIncludeJarPattern", ".*\\.jar|.*/classes/.*");
-        context.setConfigurationDiscovered(true);
-        context.getServletContext().setExtendedListenerTypes(true);
-        context.addEventListener(new ServletContextListeners());
-
-        int port = 8080;
-        if (args.length >= 1) {
-            port = Integer.parseInt(args[0]);
-        }
-        server = new Server(port);
-        server.setHandler(context);
-        server.start();
-        System.out.println("\n\n=================================================\n\n" +
-                "Please open http://localhost:" + port + " in your browser\n\n" +
-                "If you see the 'Unable to determine mode of operation' exception, just kill me and run `mvn -C clean package`\n\n" +
-                "=================================================\n\n");
-    }
-
-    public static void stop() throws Exception {
-        server.stop();
-        server = null;
-    }
-
-    private static boolean isProductionMode() {
-        final String probe = "META-INF/maven/com.vaadin/flow-server-production-mode/pom.xml";
-        final ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-        return classLoader.getResource(probe) != null;
-    }
-
-    private static Resource findWebRoot() throws MalformedURLException {
-        // don't look up directory as a resource, it's unreliable: https://github.com/eclipse/jetty.project/issues/4173#issuecomment-539769734
-        // instead we'll look up the /webapp/ROOT and retrieve the parent folder from that.
-        final URL f = Main.class.getResource("/webapp/ROOT");
-        if (f == null) {
-            throw new IllegalStateException("Invalid state: the resource /webapp/ROOT doesn't exist, has webapp been packaged in as a resource?");
-        }
-        final String url = f.toString();
-        if (!url.endsWith("/ROOT")) {
-            throw new RuntimeException("Parameter url: invalid value " + url + ": doesn't end with /ROOT");
-        }
-        System.err.println("/webapp/ROOT is " + f);
-
-        // Resolve file to directory
-        URL webRoot = new URL(url.substring(0, url.length() - 5));
-        System.err.println("WebRoot is " + webRoot);
-        return Resource.newResource(webRoot);
+    public static void main(@NotNull String[] args) throws Exception {
+        new VaadinBoot().withArgs(args).run();
     }
 }
