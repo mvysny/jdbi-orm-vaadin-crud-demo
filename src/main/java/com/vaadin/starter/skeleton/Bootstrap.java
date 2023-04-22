3f379e6a5ed631a1969ec667a658cf8102b2cffb
Martin Vysny <mavi@vaadin.com>
2023-04-22 10:42:25 +0300

    #29 Bootstrap: await a bit for the database to start properly. Helps with docker-compose

--- src/main/java/com/vaadin/starter/skeleton/Bootstrap.java
+++ src/main/java/com/vaadin/starter/skeleton/Bootstrap.java
@@ -50,6 +50,10 @@ public class Bootstrap implements ServletContextListener {
         hikariConfig.setUsername(jdbcUsername);
         hikariConfig.setPassword(jdbcPassword);
         hikariConfig.setMinimumIdle(0);
+        // wait 5 seconds for the connection to become available. This is important for
+        // docker-compose since both the app and the database are starting at the same time,
+        // and we have to wait until the database becomes ready.
+        hikariConfig.setInitializationFailTimeout(5000);
         // Let's create the DataSource and set it to JDBI-ORM
         JdbiOrm.setDataSource(new HikariDataSource(hikariConfig));
         // Done! The database layer is now ready to be used.
