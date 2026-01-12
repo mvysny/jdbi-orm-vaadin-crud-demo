e31ca9c6d75627a4ef5bb7483cb93bd16d22ba31
Martin Vysny <mavi@vaadin.com>
2026-01-12 09:04:50 +0200

    initial port to Vaadin 25

--- src/main/java/com/vaadin/starter/skeleton/AppShell.java
+++ src/main/java/com/vaadin/starter/skeleton/AppShell.java
@@ -1,11 +1,15 @@
 package com.vaadin.starter.skeleton;
 
+import com.vaadin.flow.component.dependency.StyleSheet;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.server.PWA;
+import com.vaadin.flow.theme.lumo.Lumo;
 
 /**
- * Configures the main html body tag. See {@link AppShellConfigurator} for more details.
+ * Configures the main html body tag. See {@link AppShellConfigurator} for more
+ * details.
  */
 @PWA(name = "Project Base for Vaadin", shortName = "Project Base")
+@StyleSheet(Lumo.STYLESHEET)
 public class AppShell implements AppShellConfigurator {
 }
